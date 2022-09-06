15ce61008108d5c7d40666f0ba588a20956a8aca
HectorMartinezCred <hector.martinez.cred@outlook.com>
2022-09-06 18:48:05 -0500

    Service Implementation Part 2

--- src/main/java/com/brushbrosh/servermanagerspring/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/brushbrosh/servermanagerspring/service/implementation/ServerServiceImpl.java
@@ -1,14 +1,20 @@
 package com.brushbrosh.servermanagerspring.service.implementation;
 
+import static java.lang.Boolean.TRUE;
+import static com.brushbrosh.servermanagerspring.enumeration.Status.SERVER_UP;
+import static com.brushbrosh.servermanagerspring.enumeration.Status.SERVER_DOWN;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.brushbrosh.servermanagerspring.enumeration.Status;
 import com.brushbrosh.servermanagerspring.model.Server;
 import com.brushbrosh.servermanagerspring.repository.ServerRepository;
 import com.brushbrosh.servermanagerspring.service.ServerService;
@@ -36,37 +42,40 @@ public class ServerServiceImpl implements ServerService{
         log.info("Pinging server IP: {}", ipAddress);
         Server server = serverRepository.findByIpAddress(ipAddress);
         InetAddress address = InetAddress.getByName(ipAddress);
-        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
         serverRepository.save(server);
         return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        // TODO Auto-generated method stub
-        return null;
+        log.info("Feching all servers");
+        return serverRepository.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+        log.info("Feching server by ID: {}", id);
+        return serverRepository.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        // TODO Auto-generated method stub
-        return null;
+        log.info("Updating server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+        log.info("Deleting server by ID: {}", id);
+        serverRepository.deleteById(id);
+        return TRUE;
     }
 
     private String setServerImageUrl() {
-        return null;
+        String[] imageNames = {"server1.png", "server2.png", "server3.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image" + imageNames[new Random().nextInt(3)]).toUriString();
     }
 
 }
