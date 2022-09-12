5d91f1cb23161e74c8a62742f3e7b051e31fd7e4
HectorMartinezCred <hector.martinez.cred@outlook.com>
2022-09-12 14:19:19 -0500

    HTTP Resources Part 2

--- src/main/java/com/brushbrosh/servermanagerspring/ServermanagerSpringApplication.java
+++ src/main/java/com/brushbrosh/servermanagerspring/ServermanagerSpringApplication.java
@@ -1,13 +1,28 @@
 package com.brushbrosh.servermanagerspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.brushbrosh.servermanagerspring.enumeration.Status;
+import com.brushbrosh.servermanagerspring.model.Server;
+import com.brushbrosh.servermanagerspring.repository.ServerRepository;
 
 @SpringBootApplication
 public class ServermanagerSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServermanagerSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return arg -> {
+			serverRepository.save(new Server(null, "10.0.10.67", "Ubuntu Server", "16 GB", "GLPI Server", "http:/localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "10.0.10.132", "Windows Server 2019", "16 GB", "License Server", "http:/localhost:8080/server/image/server2.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "10.0.12.142", "Ubuntu Server", "16 GB", "GLPI Server Test", "http:/localhost:8080/server/image/server3.png", Status.SERVER_UP));
+		};
 	}
 
 }
--- src/main/java/com/brushbrosh/servermanagerspring/resource/ServerResource.java
+++ src/main/java/com/brushbrosh/servermanagerspring/resource/ServerResource.java
@@ -1,13 +1,17 @@
 package com.brushbrosh.servermanagerspring.resource;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,5 +70,36 @@ public class ServerResource {
                     .statusCode(HttpStatus.CREATED.value())
                     .build()
         );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+            Response.builder()
+                    .timeStamp(LocalDateTime.now())
+                    .data(Map.of("server", serverServiceImpl.get(id)))
+                    .message("Server retrieved")
+                    .status(HttpStatus.OK)
+                    .statusCode(HttpStatus.OK.value())
+                    .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+            Response.builder()
+                    .timeStamp(LocalDateTime.now())
+                    .data(Map.of("deleted", serverServiceImpl.get(id)))
+                    .message("Server deleted")
+                    .status(HttpStatus.OK)
+                    .statusCode(HttpStatus.OK.value())
+                    .build()
+        );
+    }
+
+    @GetMapping(path = "/image/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get("C:/Workspace/Back-end/servermanager-spring/src/main/resources/static/png/" + fileName));
     }
 }
